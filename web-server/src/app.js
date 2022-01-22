9053d03ff42018adae48b8ff95c99359c31c00f4
Shivam Rajvir <shivamrajvir@gmail.com>
2022-01-22 15:17:20 +0530

    Changes for setuo for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -67,10 +68,12 @@ app.get('/weather', (req, res) => {
                 }
 
                 console.log(location)
-                console.log(forecastData)
+                if (forecastData)
+                    console.log(forecastData)
                 res.send({
                     location: location,
-                    forecast: forecastData
+                    forecast: forecastData,
+                    address: req.query.address
                 })
             })
         })
@@ -125,6 +128,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
 })
